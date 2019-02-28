d7cc00dac966545370952c672c5d996615cf90d0
hutchinsonn <34116797+hutchinsonn@users.noreply.github.com>
2019-02-28 08:36:12 -0500

    Update Wavestest.ino

--- Wavestest.ino
+++ Wavestest.ino
@@ -1,37 +1,21 @@
 #include <FastLED.h>
-
 #define NUM_LEDS 135
-
 #define DATA_PIN 11
-
-
 
 CRGB leds[NUM_LEDS];
 
-
-
 void setup() {
-
   FastLED.addLeds<WS2812B, DATA_PIN, GRB>(leds, NUM_LEDS);
-
 }
 
 void loop() {
-
   //make blue up to LED#22 with the leading one being White.
-
 for (int n = 22; n<NUM_LEDS;n+= 22){
-
   for(int i=0; i < n; i++){
-
     leds[i] = CRGB::White;
-
     FastLED.show();
-
     delay(30);
-
     leds[i] = CRGB::Blue;
-
   }
   for(int i=0;i<n;i++){
     leds[n-i] = CRGB::White;
